2821062b56c0620728142d2083206fe42298940e
骇人的籽 <60039041+JoshuaYu-crash@users.noreply.github.com>
2020-06-25 16:14:11 +0800

    提交初步配置

---
+++ app/settings.py
@@ -0,0 +1,11 @@
+# MySQL数据库连接
+SQLALCHEMY_DATABASE_URI = "mysql://root:123456@127.0.0.1/data"
+
+# 数据库追踪关闭
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# 密钥设置
+SECRET_KEY = "SecretKey"
+
+# JSON自动排序关闭
+JSON_SORT_KEYS = False
