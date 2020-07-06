7d3df344e515d9955c21760f50771d2458d78ab8
骇人的籽 <60039041+JoshuaYu-crash@users.noreply.github.com>
2020-07-06 16:24:15 +0800

    improve the doc of the anime part

--- app/Admin/form.py
+++ app/Admin/form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, StringField, PasswordField
+from wtforms import SubmitField, StringField, PasswordField, FileField
 from wtforms.validators import DataRequired
 
 class adminLoginForm(FlaskForm):
@@ -24,3 +24,50 @@ class adminLoginForm(FlaskForm):
         }
     )
     submit = SubmitField(u'登录')
+
+
+class finishedAnimeForm(FlaskForm):
+    bvid = StringField(
+        label=u'bvid',
+        validators=[
+            DataRequired(u"请输入bvid")
+        ],
+        render_kw={
+            "placeholder": "请输入bvid",
+            "required": False
+        }
+    )
+    title = StringField(
+        label=u'标题',
+        validators=[
+            DataRequired(u"请输入标题")
+        ],
+        render_kw={
+            "placeholder": "请输入标题",
+            "required": False
+        }
+    )
+    introduce = StringField(
+        label=u'介绍',
+        validators=[
+            DataRequired(u"请输入介绍")
+        ],
+        render_kw={
+            "placeholder": "请输入介绍",
+            "required": False
+        }
+    )
+    picture = FileField(
+        label=u'封面',
+    )
+    createtime = StringField(
+        label=u'时间',
+        validators=[
+            DataRequired(u"请输入时间")
+        ],
+        render_kw={
+            "placeholder": "请输入时间",
+            "required": False
+        }
+    )
+    submit = SubmitField(u'提交')
--- app/Admin/index.py
+++ app/Admin/index.py
@@ -1,39 +1,62 @@
+from flask import redirect, url_for, render_template, jsonify
+
 from app.Admin import admin
 from app.Admin.login import adminLoginConfirm
-from app.Admin.form import *
-from flask import flash, session, redirect, url_for, render_template, request
 from app.models import *
-from app.utils import *
+from app.Admin.form import *
+from app.Admin.AnimeDatas import Animedatas
 
 
 @admin.route('/index/<name>')
 def adminIndex(name):
     adminLoginConfirm(name)
-    finishedAnimeDatas = finishedAnime.query.filter_by(isShow=True)
-    return render_template('adminIndex.html', name=name, finishedAnimeDatas=finishedAnimeDatas)
+    AnimeDatas = Anime.query.filter_by(isShow=True)
+    addfinishedAnimeForm = finishedAnimeForm()
+    return render_template(
+        'adminIndex.html',
+        name=name,
+        finishedAnimeDatas=AnimeDatas,
+        addfinishedAnimeForm=addfinishedAnimeForm
+    )
 
 
+# 刷新完结动画推荐
+# 原先的不会删除，会被标记为不显示
 @admin.route('/index/refreshfinishedanime/<name>', methods=['get'])
 def refreshFinishedAnime(name):
-    adminLoginConfirm(name)
+    # adminLoginConfirm(name)
 
-    oldDatas = finishedAnime.query.filter_by(isShow=True)
-    for oldData in oldDatas:
-        oldData.isShow = False
-    datas = getMsg(url1)
+    # 设置原先不显示
+    # oldDatas = Anime.query.filter_by(isShow=True, datafrom=1)
+    # for oldData in oldDatas:
+    #     oldData.isShow = False
+
+    # 添加爬取的新数据
+    datas = Animedatas
     for data in datas:
-        newData = finishedAnime(
-            bvid=data['bvid'],
+        newData = Anime(
             title=data['title'],
             picture=data['picture'],
-            introduce=data['introduce'],
-            createtime=data['createtime']
+            describe=data['describe'],
+            seasonId=data['seasonId'],
+            mediaId=data['mediaId'],
+            link=data['link'],
+            isFinish=data['isFinish'],
+            tag1=data['tag'],
+            datafrom=1,
+            isShow=True
         )
         db.session.add(newData)
     db.session.commit()
     return redirect(url_for('admin.adminIndex', name=name))
 
 
+# 删除完结动画推荐
+@admin.route('/index/deletefinishedanime/<name>/<int:id>', methods=['GET'])
+def deleteFinishedAnime(name, id):
+    adminLoginConfirm(name)
 
-
-
+    data = Anime.query.get(id)
+    data.isShow = False
+    db.session.commit()
+    return redirect(url_for('admin.adminIndex', name=name))
