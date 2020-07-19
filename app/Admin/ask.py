71b71c215a97828777f70844e28e8cbfd42a830b
骇人的籽 <60039041+JoshuaYu-crash@users.noreply.github.com>
2020-07-19 16:32:18 +0800

    finish the ask and recommend part

--- app/Admin/ask.py
+++ app/Admin/ask.py
@@ -46,12 +46,42 @@ def operateAsk(token):
     # 发邮件提醒，删番
     elif request.method == 'DELETE':
         question = request.form.get('question')
-        message = "您的标题为" + ask.title + "的问番因" + question + "被管理员删除"
+        message = "您的标题为" + ask.title + "的问番因" + question + "被管理员删除，特此予以警告"
+        db.session.delete(ask)
+        db.session.commit()
         send_email("警告", ask.user.email, message)
+        return jsonify(Event0(token=token))
 
 
-@admin.route('/askcomment')
+@admin.route('/askcomment', methods=['PSOT', 'DELETE'])
 @admin_login
 # 查，删
 def askComment(token):
-    pass
+    adminId = r.get(token)
+    admin = Admin.query.get(adminId)
+    commentId = request.form.get('commentid')
+    comment = Drama.query.get(commentId)
+    if request.method == 'POST':
+        data = {
+            "commentid": comment.id,
+            "dramatitle": comment.drama.title,
+            "dramaid": comment.drama.id,
+            "authorname": comment.author,
+            "authorid": comment.author_id,
+            "content": comment.text,
+            "time": comment.time
+        }
+        return jsonify(
+            {
+                "status": 0,
+                "data": data
+            }
+        )
+    elif request.method == 'DELETE':
+        question = request.form.get('question')
+        message = "您的在标题为" + comment.drama.title + "下的的评论因" + question + "被管理员删除，特此予以警告"
+        user = User.query.get(comment.author_id)
+        send_email("警告", user.email, message)
+        db.session.delete(comment)
+        db.session.commit()
+        return jsonify(Event0(token=token))
--- app/Admin/recommend.py
+++ app/Admin/recommend.py
@@ -32,7 +32,7 @@ def getAllRecommend(token):
 def operateRecommend(token):
     adminId = r.get(token)
     admin = Admin.query.get(adminId)
-    recommendId = request.form.get('askid')
+    recommendId = request.form.get('recommendid')
     recommend = Drama.query.get(recommendId)
     # 获取当个详细
     if request.method == 'POST':
@@ -46,12 +46,42 @@ def operateRecommend(token):
     # 发邮件提醒，删番
     elif request.method == 'DELETE':
         question = request.form.get('question')
-        message = "您的标题为" + recommend.title + "的问番因" + question + "被管理员删除"
+        message = "您的标题为" + recommend.title + "的问番因" + question + "被管理员删除，特此予以警告"
+        db.session.delete(recommend)
+        db.session.commit()
         send_email("警告", recommend.user.email, message)
+        return jsonify(Event0(token=token))
 
 
-@admin.route('/recommendcomment')
+@admin.route('/recommendcomment', methods=['PSOT', 'DELETE'])
 @admin_login
 # 查，删
 def recommendComment(token):
-    pass
+    adminId = r.get(token)
+    admin = Admin.query.get(adminId)
+    commentId = request.form.get('commentid')
+    comment = Drama.query.get(commentId)
+    if request.method == 'POST':
+        data = {
+            "commentid": comment.id,
+            "dramatitle": comment.drama.title,
+            "dramaid": comment.drama.id,
+            "authorname": comment.author,
+            "authorid": comment.author_id,
+            "content": comment.text,
+            "time": comment.time
+        }
+        return jsonify(
+            {
+                "status": 0,
+                "data": data
+            }
+        )
+    elif request.method == 'DELETE':
+        question = request.form.get('question')
+        message = "您的在标题为" + comment.drama.title + "下的的评论因" + question + "被管理员删除，特此予以警告"
+        user = User.query.get(comment.author_id)
+        send_email("警告", user.email, message)
+        db.session.delete(comment)
+        db.session.commit()
+        return jsonify(Event0(token=token))
